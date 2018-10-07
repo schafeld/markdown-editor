e2b58adb67959977a3bc905cb3b774ed0add4ad8
Oliver Schafeld <oliver@schafeld.com>
2018-10-07 19:22:21 +0100

    add basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
   <div class="main">
-      <div class="sidebar"></div>
+    <p>Work in progress...</p>
+      <div class="sidebar">(Sidebar)</div>
+      <div class="content">
+        <div class="editor">(Editor)</div>
+        <div class="renderer">(Renderer)</div>
+      </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style scoped>
+  .main {
+    height: 100vh;
+    display: flex;
+    overflow: hidden;
+  }
+  .sidebar {
+    flex-basis: 20%;
+    background-color: #eee;
+  }
+  .content {
+    flex-basis: 80%;
+    display: flex;
+  }
+  .editor,
+  .renderer {
+    flex-basis: 50%;
+  }
+  .editor{
+    border-right: 2px solid #ddd;
+  }
 </style>
--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -2,10 +2,29 @@ import Vue from 'vue'
 import App from '@/App'
 
 describe('App.vue', () => {
-  test('it should have a sidebar', () => {
+  let vm
+  beforeAll(() => {
     const Constructor = Vue.extend(App)
-    const vm = new Constructor().$mount()
+    vm = new Constructor().$mount()
+  })
+
+  test('it should have a sidebar', () => {
     expect(vm.$el.querySelector('.sidebar'))
+      .not
+      .toBeNull()
+  })
+  test('it should have a content panel', () => {
+    expect(vm.$el.querySelector('.content'))
+      .not
+      .toBeNull()
+  })
+  test('it\'s content panel should have an editor', () => {
+    expect(vm.$el.querySelector('.content .editor'))
+      .not
+      .toBeNull()
+  })
+  test('it\'s content panel should have an renderer', () => {
+    expect(vm.$el.querySelector('.content .renderer'))
       .not
       .toBeNull()
   })
